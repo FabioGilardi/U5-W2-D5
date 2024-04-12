55ed156871aeb654cdbfcc06c15ad419aa759c6e
Fabio Gilardi <fabiogilardi02@gmail.com>
2024-04-12 11:37:07 +0200

    DeviceService fixed

--- src/main/java/FabioGilardi/U5W2D5/services/DeviceService.java
+++ src/main/java/FabioGilardi/U5W2D5/services/DeviceService.java
@@ -3,6 +3,7 @@ package FabioGilardi.U5W2D5.services;
 import FabioGilardi.U5W2D5.entities.Device;
 import FabioGilardi.U5W2D5.exceptions.BadRequestException;
 import FabioGilardi.U5W2D5.exceptions.NotFoundException;
+import FabioGilardi.U5W2D5.payloads.DeviceAssignementDTO;
 import FabioGilardi.U5W2D5.payloads.DeviceDTO;
 import FabioGilardi.U5W2D5.repositories.DeviceDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +11,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DeviceService {
 
     @Autowired
     private DeviceDAO deviceDAO;
+
+    @Autowired
+    private EmployeeService employeeService;
 
     public Page<Device> findAll(int number, int size, String sortBY) {
         Pageable pageable = PageRequest.of(number, size, Sort.by(sortBY));
@@ -45,5 +51,13 @@ public class DeviceService {
     public void findByIdAndDelete(long id) {
         Device found = this.findById(id);
         deviceDAO.delete(found);
+    }
+
+    public Device findByIdAndAssign(long id, DeviceAssignementDTO payload) {
+        Device found = this.findById(id);
+        found.setStatus("assigned");
+        found.setEmployee(employeeService.findById(payload.employeeId()));
+        deviceDAO.save(found);
+        return found;
     }
 }
